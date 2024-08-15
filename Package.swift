ed82c103f9dbde1bba3604f2b64dc594a82f14d7
Chad Seippel <chseippe@microsoft.com>
2024-08-15 16:55:45 -0400

    SPM

---
+++ Package.swift
@@ -0,0 +1,31 @@
+let package = Package(
+    name: "OneDS",
+    platforms: [
+        .iOS(.v10),
+    ],
+    products: [
+        .library(
+            name: "OneDS",
+            type: .dynamic,
+            targets: ["OneDS"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "OneDS",
+            dependencies: [],
+            path: "./lib",
+            publicHeadersPath: "/lib/include/public",
+            cSettings: [
+            ],
+            linkerSettings: [
+                .linkedFramework("Network"),
+                .linkedFramework("SystemConfiguration"),
+                .linkedFramework("UIKit"),
+                .linkedLibrary("stdc++"),
+                .linkedLibrary("z"),
+                .linkedLibrary("sqlite3")
+            ])
+    ]
+)
